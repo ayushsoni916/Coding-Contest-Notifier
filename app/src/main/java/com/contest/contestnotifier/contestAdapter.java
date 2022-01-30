a8f459eaf635a4a90fe37c19ebcd835e10c80441
Ayush Soni <ayush.20bcrn052@jecrcu.edu.in>
2022-01-30 23:17:58 +0530

    contest data improved

--- app/src/main/java/com/contest/contestnotifier/contestAdapter.java
+++ app/src/main/java/com/contest/contestnotifier/contestAdapter.java
@@ -1,15 +1,24 @@
 package com.contest.contestnotifier;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 public class contestAdapter extends RecyclerView.Adapter<contestAdapter.MyViewHolder> {
@@ -30,18 +39,39 @@ public class contestAdapter extends RecyclerView.Adapter<contestAdapter.MyViewHo
         return viewHolder;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.name.setText(datalist.get(position).getName());
-        holder.url.setText(datalist.get(position).getUrl());
+//        String s=datalist.get(position).getStatus();
 //        Toast.makeText(context, postPojo.class.getName(), Toast.LENGTH_SHORT).show();
-        holder.starTime.setText(datalist.get(position).getStart_time());
-        holder.endTime.setText(datalist.get(position).getEnd_time());
-        holder.duration.setText(datalist.get(position).getDuration());
+        String s =datalist.get(position).getStart_time();
+        String e =datalist.get(position).getEnd_time();
+//        String a=p.substring(0,10);
+        Instant timestart = Instant.parse(s.substring(0,10)+"T"+s.substring(11,19)+"Z");
+        Instant timeend = Instant.parse(e.substring(0,10)+"T"+e.substring(11,19)+"Z");
+        ZoneId.of("Asia/Kolkata");
+        LocalDateTime l = LocalDateTime.ofInstant(timestart, ZoneId.of("Asia/Kolkata"));
+        LocalDateTime p = LocalDateTime.ofInstant(timeend, ZoneId.of("Asia/Kolkata"));
+        holder.starTime.setText("Start : "+l.toString().substring(0,10)+" "+l.toString().substring(11));
+         holder.endTime.setText("End    : "+p.toString().substring(0,10)+" "+p.toString().substring(11));
+//        holder.duration.setText(datalist.get(position).getDuration());
 //        holder.ratedRange.setText(datalist.get(position).get);
-        holder.status.setText(datalist.get(position).getStatus());
+//        if (s == "")
+        holder.status.setText(s);
         holder.time.setText(datalist.get(position).getIn_24_hours());
-//        holder.status.setText(datalist.get(position).getName());
+        holder.share.setImageResource(R.drawable.share);
+
+        holder.share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse(datalist.get(position).getUrl());
+                Intent i =new Intent(Intent.ACTION_VIEW,uri);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
+            }
+        });
+        holder.status.setText(datalist.get(position).getName());
     }
 
     @Override
@@ -49,19 +79,23 @@ public class contestAdapter extends RecyclerView.Adapter<contestAdapter.MyViewHo
         return datalist.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView name,url,starTime , endTime,duration
+    public class MyViewHolder extends RecyclerView.ViewHolder  {
+        TextView name,starTime , endTime
                 ,status , time;
+        ImageView share;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+//            itemView.setOnClickListener(this);
             name=itemView.findViewById(R.id.name);
-            url=itemView.findViewById(R.id.url);
             starTime=itemView.findViewById(R.id.start_time);
             endTime=itemView.findViewById(R.id.end_time);
-            duration=itemView.findViewById(R.id.duration);
+//            duration=itemView.findViewById(R.id.duration);
 //            ratedRange=itemView.findViewById(R.id.rated_range);
             status=itemView.findViewById(R.id.status);
             time=itemView.findViewById(R.id.in_24_hr);
+            share = itemView.findViewById(R.id.share);
         }
+
+
     }
 }
